7965b1bac4f82873979063965a5ded74084901d7
Mohib Mohammed <23mmohammed@heckgrammar.co.uk>
2025-06-24 06:57:19 +0100

    Done homework

--- ExtractingStringPartsTask/Program.cs
+++ ExtractingStringPartsTask/Program.cs
@@ -9,19 +9,21 @@
             string city = "San Fransicso";
             string landmark = "Alcatraz Island";
             // output the length of state
-
+            Console.WriteLine(state.Length);
             // Concatenate the contents of city and landmark * don't forget to add a space
-
+            Console.WriteLine(city + "" + landmark);
             // output the first character of county
-
+            Console.WriteLine(country[0]);
             // output a concatenation of the first and last characters of city
-
+            Console.WriteLine(city[0].ToString() + city[city.Length - 1]);
             // output the substring of landmark starting at c and outputting to the end
-
+            int cIndex = landmark.IndexOf('c');
+            Console.WriteLine(landmark.Substring(cIndex));
             // output the substring of country starting at the first S and ending at the first A
-
+            int sIndex = country.IndexOf('S');
+            int aIndex = country.IndexOf('A');
             // output the position of f in California
-
+            Console.WriteLine(state.IndexOf('f'));
             //************CHALLENGE****************//
             // Output how many vowels are in each of the strings in the format
             // California has n vowels
